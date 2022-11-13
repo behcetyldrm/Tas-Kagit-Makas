324cb52e2460ca3b2e8bfbcc1a6e0284e7e62917
Behçet Emre <76630686+behcetyldrm@users.noreply.github.com>
2022-11-13 14:07:26 +0300

    Add files via upload

---
+++ tas_kagit_makas.py
@@ -0,0 +1,79 @@
+import random
+
+secenek = ["taş", "kağıt", "makas"]
+tas = secenek[0]
+kagit = secenek[1]
+makas = secenek[2]
+bilgisayarSkor = 0
+benimSkor = 0
+
+print("******TAŞ, KAĞIT, MAKAS OYUNUNA HOŞGELDİN!******")
+while True:
+
+    bilgisayarSecim = random.choice(secenek)
+    benimSecim = input("\nTaş mı, kağıt mı, makas mı ?: ")
+
+    if benimSecim == tas:
+
+        if bilgisayarSecim == tas:
+            print("Sonuç: Berabere. Sen: Taş, Bilgisayar: Taş")
+            
+        elif bilgisayarSecim == kagit:
+            print("Sonuç: Bilgisayar Galip. Sen: Taş, Bilgisayar: Kağıt")
+            bilgisayarSkor += 1
+            
+        elif bilgisayarSecim == makas:
+            print("Sonuç: Galipsin. Sen: Taş, Bilgisayar: Makas")
+            benimSkor += 1
+            
+    if benimSecim == kagit:
+
+        if bilgisayarSecim == tas:
+            print("Sonuç: Galipsin. Sen: Kağıt, Bilgisayar: Taş")
+            benimSkor += 1
+        
+        elif bilgisayarSecim == kagit:
+            print("Sonuç: Berabere. Sen: Kağıt, Bilgisayar: Kağıt")
+            
+        elif bilgisayarSecim == makas:
+            print("Sonuç: Bilgisayar Galip. Sen: Kağıt, Bilgisayar: Makas")
+            bilgisayarSkor += 1
+            
+    if benimSecim == makas:
+
+        if bilgisayarSecim == tas:
+            print("Sonuç: Bilgisayar Galip. Sen: Makas, Bilgisayar: Taş")
+            bilgisayarSkor += 1
+            
+        elif bilgisayarSecim == kagit:
+            print("Sonuç: Galipsin. Sen: Makas, Bilgisayar: Kağıt")
+            benimSkor += 1
+            
+        elif bilgisayarSecim == makas:
+            print("Sonuç: Berabere. Sen = Makas, Bilgisayar: Makas")
+    
+    if benimSkor == 3:
+        print(f"Tebrikler kazandın! \nSen: {benimSkor}, Bilgisayar: {bilgisayarSkor}")
+        
+        devam = input("\nTekrar oynamak ister misin? (e/h): ")
+
+        if devam == "e":
+            bilgisayarSkor = 0
+            benimSkor = 0
+            continue
+        elif devam == "h":
+            print("hoşçakal...")
+            break
+
+    elif bilgisayarSkor == 3:
+        print(f"Maalesef kaybettin! \nSen: {benimSkor}, Bilgisayar: {bilgisayarSkor}")
+
+        devam = input("\nTekrar oynamak ister misin? (e/h): ")
+
+        if devam == "e":
+            bilgisayarSkor = 0
+            benimSkor = 0
+            continue
+        elif devam == "h":
+            print("hoşçakal...")
+            break
